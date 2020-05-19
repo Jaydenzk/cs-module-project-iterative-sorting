bb7696bb3d47db631a0da7a7a92042c9d687ee31
Jaydenzk <joowan88@gmail.com>
2020-05-19 12:57:37 -0700

    updated

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,10 +7,13 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range(i + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
@@ -18,6 +21,20 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    k = 0
+    for _ in range(0, len(arr) - (1)):
+        swapped = False
+        # Loop through your array
+        for i in range(0, len(arr) - (1 + k)):
+            # Compare each element to its neighbor
+            # If elements in wrong position (relative to each other, swap them)
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i + 1], arr[i]
+                swapped = True
+        k = k + 1
+        # If no swaps performed, stop.
+        if swapped == False:
+            break
 
 
     return arr
@@ -26,6 +43,30 @@ def bubble_sort(arr):
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
+    if len(arr) == 0:
+        return []
+
+    # set up k + 1 buckets, where k is max value in arr
+    maximum = max(arr)
+    count = [0 for _ in range(maximum + 1)]
+
+    # increment the count for each index for every corresponding value in arr
+    for item in arr:
+        if item < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+
+        count[item] = count[item] + 1
+
+    # modify the count arr to have a running total of items
+    for i in range(1, len(count)):
+        count[i] = count[i] + count[i - 1]
+
+    # for each item in arr, use the count arr value at that index to place item,
+    # then decrement the value in count arr by 1
+    arr_sorted = [None for _ in range(len(arr))]
+    for item in arr:
+        arr_sorted[count[item] - 1] = item
+        count[item] = count[item] - 1
 
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,8 @@
 def linear_search(arr, target):
     # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -9,6 +12,20 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    if len(arr) == 0:
+        return -1 # array empty
+
+    low = 0
+    high = len(arr)-1
+
+    while high >= low:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
 
 
     return -1  # not found
